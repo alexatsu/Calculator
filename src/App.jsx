7916ac9e6307d2266064319654540f17b2cb4251
AlexanderKudr <trevan98@gmail.com>
2022-09-13 03:46:52 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.scss'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='app'>
+      <p className='text'>ik</p>
+    </div>
+  )
+}
+
+export default App
